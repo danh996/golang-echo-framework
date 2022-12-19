1d371f32f0c1669861b85eaa5f44a6541f593680
Le Cong Danh <congdanh.le@manabie.com>
2022-12-19 22:08:55 +0700

    load config and connect to Postgres db

--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"database/sql"
+	"log"
+
 	"github.com/danh996/golang-echo-framework/cmd/api/handlers"
+	"github.com/danh996/golang-echo-framework/util"
 	"github.com/labstack/echo"
+	_ "github.com/lib/pq"
+
 )
 
 func main() {
+
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("can not load config", err)
+	}
+	_, err = sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("can not connect to DB", err)
+	}
+
 	e := echo.New()
 	e.GET("/health-check", handlers.HealthCheckHandler)
 	e.GET("/posts", handlers.PostIndexHandler)
