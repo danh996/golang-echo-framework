51e3f094cab609ec3c5390cb9c0e01b4ce19387b
Le Cong Danh <congdanh.le@manabie.com>
2022-12-19 22:01:47 +0700

    get posts list and post detail success

--- cmd/api/handlers/post_handler.go
+++ cmd/api/handlers/post_handler.go
@@ -1,11 +1,40 @@
 package handlers
 
-import "github.com/labstack/echo"
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/danh996/golang-echo-framework/cmd/api/service"
+
+	"github.com/labstack/echo"
+)
 
 func PostIndexHandler(c echo.Context) error {
+	data, err := service.GetAll()
+	if err != nil {
+		c.String(http.StatusBadGateway, "Unable to process data")
+	}
+	res := make(map[string]any)
+	res["status"] = "ok"
+	res["data"] = data
 
+	return c.JSON(http.StatusOK, res)
 }
 
 func PostDetailHandler(c echo.Context) error {
+	id := c.Param("id")
+	idx, err := strconv.Atoi(id)
+	if err != nil {
+		c.String(http.StatusBadGateway, "Unable to process data")
+	}
 
+	data, err := service.GetById(idx)
+	if err != nil {
+		c.String(http.StatusBadGateway, "Unable to process data")
+	}
+	res := make(map[string]any)
+	res["status"] = "ok"
+	res["data"] = data
+
+	return c.JSON(http.StatusOK, res)
 }
--- cmd/api/handlers/post_handler_test.go
+++ cmd/api/handlers/post_handler_test.go
@@ -1,1 +1,27 @@
 package handlers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type EndToEndSuite struct {
+	suite.Suite
+}
+
+func TestEndToEndSuite(t *testing.T) {
+	suite.Run(t, new(EndToEndSuite))
+}
+
+func (s *EndToEndSuite) TestPostHandler() {
+	c := http.Client{}
+	r, _ := c.Get("http://localhost:1323/posts")
+	s.Equal(http.StatusOK, r.StatusCode)
+}
+
+func (s *EndToEndSuite) TestPostNoResult() {
+	c := http.Client{}
+	r, _ := c.Get("http://localhost:1323/post/1552")
+	s.Equal(http.StatusOK, r.StatusCode)
+}
